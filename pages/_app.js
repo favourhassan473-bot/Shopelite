443c12af696e1d8c5000d282b6e3da2084e39d21
hassan favour <favourhassan473@gmail.com>
2026-06-18 22:16:29 +0100

    Fix _app.js naming

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-﻿import { SessionProvider } from "next-auth/react";
+import { SessionProvider } from "next-auth/react";
 import { CartProvider } from "../context/CartContext";
 
 export default function App({
